cff33b645b00e2e55ec8b5fa9b93456c188bcb23
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-05-06 21:54:13 -0400

    Fixed code problems found with linter

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  const signUpForm = $('form.signup');
+  const emailInput = $('input#email-input');
+  const passwordInput = $('input#password-input');
 
-  // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  // When the signup button is clicked,
+  // we validate the email and password are not blank
+  signUpForm.on('submit', function(event) {
     event.preventDefault();
-    var userData = {
+    const userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
     };
 
     if (!userData.email || !userData.password) {
@@ -17,26 +18,36 @@ $(document).ready(function() {
     }
     // If we have an email and password, run the signUpUser function
     signUpUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
+    emailInput.val('');
+    passwordInput.val('');
   });
 
-  // Does a post to the signup route. If successful, we are redirected to the members page
-  // Otherwise we log any errors
+  // Does a post to the signup route. If successful, we are redirected 
+  // to the members page.  Otherwise we log any errors
+  /**
+ * This signs up a new User.
+ * @param {string} email The email address.
+ * @param {string} password The password.
+ *
+ */
   function signUpUser(email, password) {
-    $.post("/api/signup", {
+    $.post('/api/signup', {
       email: email,
-      password: password
+      password: password,
     })
-      .then(function(data) {
-        window.location.replace("/members");
+        .then(function(data) {
+          window.location.replace('/members');
         // If there's an error, handle it by throwing up a bootstrap alert
-      })
-      .catch(handleLoginErr);
+        })
+        .catch(handleLoginErr);
   }
 
+  /**
+ * This signs up a new User.
+ * @param {object} err The error object.
+ */
   function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
+    $('#alert .msg').text(err.responseJSON);
+    $('#alert').fadeIn(500);
   }
 });
