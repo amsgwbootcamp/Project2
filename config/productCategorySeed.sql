615bd780b405c79a79e59cb3ec5f52ca839e5747
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-05-11 20:46:15 -0400

    Added image to webpages and css

---
+++ config/productCategorySeed.sql
@@ -0,0 +1,3 @@
+INSERT INTO ProductCategory (category) VALUES ("Fresh vegetables");
+INSERT INTO ProductCategory (category) VALUES ("Fresh fruits");
+INSERT INTO ProductCategory (category) VALUES ("Fresh fruits");
