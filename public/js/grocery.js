fda687e8f25ffbc83fc123af7b50862ffb35a28d
Spunkyfrog93 <60330400+Spunkyfrog93@users.noreply.github.com>
2020-05-13 18:58:12 -0400

    redo

--- public/js/grocery.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var Grocery = sequelize.define("Grocery", {
-      text: DataTypes.STRING,
-      complete: DataTypes.BOOLEAN
-    });
-    return Grocery;
-  };
-  
