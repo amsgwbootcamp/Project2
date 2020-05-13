a3079068d88e816a04971901d244ff8c149b84fc
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-05-13 17:44:16 -0400

    Fixed problem with build

--- public/js/members.js
+++ public/js/members.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
       allowMultiple: true,
     }).modal('show');
   });
+  // Adding a comment so file will be marked as changed.  
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get('/api/user_data').then(function(data) {
