fd58dd81863b06b0055de2a7b64849be2fbc6e76
Spunkyfrog93 <60330400+Spunkyfrog93@users.noreply.github.com>
2020-05-16 21:01:24 -0400

    working grocery list

---
+++ public/js/category.js
@@ -0,0 +1,11 @@
+// $(function() {
+//   $('.category').on('click', function(event) {
+//   // eslint-disable-next-line no-invalid-this
+//     const category = $(this).data('id');
+//     app.set('id', category);
+
+//     $.get({url: `/product/${category}`}).then()
+//     alert('button has been clicked' + category);
+//   // window.location.replace('/category');
+//   });
+// });
